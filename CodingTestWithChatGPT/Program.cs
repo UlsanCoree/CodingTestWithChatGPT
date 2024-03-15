100d95db906ab592cf344a1ae5125ffb3baf7898
Kim SeungHyun <korafa@naver.com>
2024-03-15 14:23:23 +0900

    초기 커밋.

---
+++ CodingTestWithChatGPT/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CodingTestWithChatGPT
+{
+    internal class Program
+    {
+        public static int[] numbers = { 3, 9, 4, 1, 10, 50, 20, 2 };
+        static void Main(string[] args)
+        {
+            int max = GetMaxNum();
+
+            Console.WriteLine($"최대값은 : {max}");
+        }
+
+        public static int GetMaxNum()
+        {
+            int maxNum = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maxNum)
+                {
+                    maxNum = numbers[i];
+                }
+            }
+
+            return maxNum;
+        }
+    }
+}
+
